d8e3f817162093f3ea9976cc885c27b6c7bd0c94
alexus37 <alexlelidis@gmx.de>
2020-08-16 13:24:58 +0200

    add production build

---
+++ docs/precache-manifest.36111c0e55181e040f4036a9b2876fae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20021013b384d1b25a9115860cbda149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07b884685e432b8c43b3",
+    "url": "/static/css/2.57cbbca9.chunk.css"
+  },
+  {
+    "revision": "d6ae41d5ce5f8ac77193",
+    "url": "/static/css/main.0fee167c.chunk.css"
+  },
+  {
+    "revision": "07b884685e432b8c43b3",
+    "url": "/static/js/2.020f1dd6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.020f1dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ae41d5ce5f8ac77193",
+    "url": "/static/js/main.4aadbc24.chunk.js"
+  },
+  {
+    "revision": "532573dbae42f2e96c26",
+    "url": "/static/js/runtime-main.e67b6db8.js"
+  },
+  {
+    "revision": "46b933e50ed074bbfc582c01d02261fb",
+    "url": "/static/media/bg.46b933e5.png"
+  }
+]);
--- src/finishGameView.js
+++ src/finishGameView.js
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
+import { Trophy } from "react-bootstrap-icons";
+import Confetti from "react-confetti";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function FinishGameView({ points }) {
   const realPoints = {};
-  Object.keys(points).map((player) => {
+  Object.keys(points).forEach((player) => {
     realPoints[player] = points[player].filter((point) => point).length;
   });
   const realValueList = Object.values(realPoints);
-  const tie = realValueList.every((value) => value === realValueList[0]);
+  const tie =
+    realValueList.every((value) => value === realValueList[0]) &&
+    realValueList.length > 1;
 
   const winnerIndex = realValueList.indexOf(Math.max(...realValueList));
 
@@ -18,12 +22,23 @@ function FinishGameView({ points }) {
 
   return (
     <div className="background">
+      <Confetti />
       <Container>
         <Row>
-          <Col md={6}>
-            {tie ? <h1>That was a tie!</h1> : <h1>The Winner is {winner}</h1>}
+          <Col md={{ span: 8, offset: 2 }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              {tie ? <h1>That was a tie!</h1> : <h1>The Winner is {winner}</h1>}
+              <Trophy color="royalblue" size={96} />
+            </div>
           </Col>
-          <Col md={6}></Col>
         </Row>
       </Container>
     </div>
