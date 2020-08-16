babb82206b12c15518815fdef6c7f83513379f42
alexus37 <alexlelidis@gmx.de>
2020-08-16 13:31:09 +0200

    Updates

---
+++ precache-manifest.fab6a7d5ac26832d6ba6d25330342d7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe25e5d385d893e64dad41329ce6a21f",
+    "url": "/bvgler/index.html"
+  },
+  {
+    "revision": "07b884685e432b8c43b3",
+    "url": "/bvgler/static/css/2.57cbbca9.chunk.css"
+  },
+  {
+    "revision": "75fea5ba0c7663fba656",
+    "url": "/bvgler/static/css/main.c9adfe3a.chunk.css"
+  },
+  {
+    "revision": "07b884685e432b8c43b3",
+    "url": "/bvgler/static/js/2.020f1dd6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bvgler/static/js/2.020f1dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fea5ba0c7663fba656",
+    "url": "/bvgler/static/js/main.a65e7f9e.chunk.js"
+  },
+  {
+    "revision": "d48dfc76683bab8d06c2",
+    "url": "/bvgler/static/js/runtime-main.d5544f3f.js"
+  },
+  {
+    "revision": "46b933e50ed074bbfc582c01d02261fb",
+    "url": "/bvgler/static/media/bg.46b933e5.png"
+  }
+]);
