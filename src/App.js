1ac12bf52697fe892cb078afdfeac34bedb0c57f
alexus37 <alexlelidis@gmx.de>
2020-08-19 22:48:17 +0200

    add g analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import ReactGA from "react-ga";
 import Game from "./game.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const trackingId = "UA-152068498-1";
+ReactGA.initialize(trackingId);
 
 function App() {
   return <Game />;
