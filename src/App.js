0dfd78892779f48034d65459e8656e065384a3fd
alexus37 <alexlelidis@gmx.de>
2020-08-21 23:38:56 +0200

    track id fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactGA from "react-ga";
 import Game from "./game.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const trackingId = "UA-152068498";
+const trackingId = "UA-152068498-1";
 ReactGA.initialize(trackingId);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
